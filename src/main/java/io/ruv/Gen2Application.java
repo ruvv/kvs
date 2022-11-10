96485da00d63b0449e033dd66253df3f89881454
ruv <brand20@yandex.ru>
2022-11-11 02:01:55 +0300

    maven and boot

---
+++ src/main/java/io/ruv/Gen2Application.java
@@ -0,0 +1,13 @@
+package io.ruv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Gen2Application.class, args);
+    }
+}
